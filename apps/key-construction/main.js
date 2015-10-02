0e24b1abdae6b0062ffc71bb9414e089c6608926
Patrick Broman <patrickbroman@Patricks-MacBook-Pro.local>
2015-10-02 15:30:45 +0200

    Added a class representing keys, and a container for drawable objects

---
+++ apps/key-construction/main.js
@@ -0,0 +1,17 @@
+
+
+function init() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 1920, 1080);
+
+    // Initialize all keys
+
+    var keys = [];
+
+    for(var i = 0; i < gAllNotes.length; i++) {
+      var key = new Key(i);
+      keys.push(key);
+    }
+}
