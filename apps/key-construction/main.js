c5635e7764d098b178a480a9c0c5f3cb8a8f48ab
Patrick Broman <patrickbroman@Patricks-MacBook-Pro.local>
2015-10-03 14:29:53 +0200

    Initial textwidget implementation

--- apps/key-construction/main.js
+++ apps/key-construction/main.js
@@ -1,8 +1,11 @@
 
+var CHROMATIC_NOTE_Y = 50;
+var CHROMATIC_NOTE_WIDTH = 80;
+var CHROMATIC_NOTE_SPACING = 10;
 
 function init() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
+    gCanvas = document.getElementById("myCanvas");
+    var ctx = gCanvas.getContext("2d");
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1920, 1080);
 
@@ -14,4 +17,31 @@ function init() {
       var key = new Key(i);
       keys.push(key);
     }
+
+    var key = keys[0];
+
+    // Populate the container
+
+    gContainer = new Container(3);
+
+    var totalWidth = CHROMATIC_NOTE_WIDTH * 12 + CHROMATIC_NOTE_SPACING * 11;
+    var chromaticNoteX = gCanvas.width/2 - totalWidth/2;
+    var chromaticNoteY = CHROMATIC_NOTE_Y;
+
+    for(var i = 0; i < gAllNotes.length; i++) {
+        var noteWidget = new TextWidget(chromaticNoteX, chromaticNoteY, CHROMATIC_NOTE_WIDTH, CHROMATIC_NOTE_WIDTH, gAllNotes[i]);
+        gContainer.addObject(noteWidget);
+        chromaticNoteX += CHROMATIC_NOTE_WIDTH + CHROMATIC_NOTE_SPACING;
+    }
+
+    window.requestAnimationFrame(render);
+    gStartTime = performance.now();
+}
+
+function render() {
+    var ctx = gCanvas.getContext("2d");
+    var time = performance.now() - gStartTime;
+    var frame = Math.floor(time*60.0/1000.0);
+    gContainer.render(ctx, frame);
+    window.requestAnimationFrame(render);
 }
