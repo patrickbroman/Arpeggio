a8d0727166ad8b0c312adcebd5f50c9ef90fc344
patrickbroman <patrick.broman@gmail.com>
2020-04-14 10:22:44 +0200

    Implemented two octaves and added midi support (and  a software synth playback thing)

--- apps/blues-min-maj/main.js
+++ apps/blues-min-maj/main.js
@@ -4,37 +4,136 @@ var RECORDING_SECONDS = Config.recordingDuration;
 
 var QUARTER_PULSE_START = 60*4;
 
-var QUARTERNOTE_WIDTH = 100;
+var NOTE_WIDTH = 60;
+var NOTE_HEIGHT = 250;
 
-var BAR_MARGIN = 20;
+var NOTE_MARGIN = 10;
 
-var QUARTERNOTE_XSTART = 1920/2 - ((4 * 4 * QUARTERNOTE_WIDTH + 3 * BAR_MARGIN) / 2);
+var DOT_WIDTH = 40;
+var DOT_MARGIN = 30;
+
+var NOTE_XSTART = 1920/2 - ((24 * NOTE_WIDTH + 23 * NOTE_MARGIN) / 2);
+var DOT_XSTART = 1920/2 - ((24 * DOT_WIDTH + 23 * DOT_MARGIN) / 2);
+
+//var LINE_HEIGHT = 80;
 var YPOS = 240; // 1080/2 - QUARTERNOTE_HEIGHT/2;
 
+var FADE_DURATION = 60.0;
 
-var barColors = [
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
+var NOTE_FADE = 3;
 
-    ["rgba(255, 255, 0, 1.0)", "rgba(220, 220, 0, 1.0)"],
-    ["rgba(255, 255, 0, 1.0)", "rgba(220, 220, 0, 1.0)"],
 
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-
-    ["rgba(255, 0, 0, 1.0)", "rgba(220, 0, 0, 1.0)"],
-
-    ["rgba(255, 255, 0, 1.0)", "rgba(220, 220, 0, 1.0)"],
-
-    ["rgba(0, 255, 0, 1.0)", "rgba(0, 220, 0, 1.0)"],
-
-    ["rgba(255, 0, 0, 1.0)", "rgba(220, 0, 0, 1.0)"]
-
+var noteFrequencies = [
+    130.81,
+    138.59,
+    146.83,
+    155.56,
+    164.81,
+    174.61,
+    185,
+    196,
+    207.65,
+    220,
+    233.08,
+    246.94,
+    261.63,
+    277.18,
+    293.66,
+    311.13,
+    329.63,
+    349.23,
+    369.99,
+    392.00,
+    415.30,
+    440.00,
+    466.16,
+    493.88
 ];
 
+depressed_keys = {};
+oscOn = false;
 
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function noteOn(index) {
+    if(!oscOn) {
+        vco.start(0);
+        context.resume();
+
+        oscOn = true;
+    }
+    console.log("NOTE ON!");
+    if(index < 0 || index > 23) {
+        return;
+    }
+    var note = gNoteWidgets[index];
+    note.backgroundOpacity = 1.0;
+    vco.frequency.value = noteFrequencies[index];
+    vca.gain.value = 1;
+    depressed_keys[index] = true;
+
+}
+
+function noteOff(index) {
+    if(index < 0 || index > 23) {
+        return;
+    }
+    var note = gNoteWidgets[index];
+    var frame = getCurrentFrame();
+    note.addAnimation(makeAnimation(
+        {
+            "backgroundOpacity" : makeInterpolator(1.0, 0.4),
+        }
+    )
+    , frame, frame + NOTE_FADE);
+
+    delete depressed_keys[index];
+    if (isEmpty(depressed_keys)) {
+        vco.frequency.value = 0;
+        vca.gain.value = 0;
+    }
+}
+
+function getCurrentFrame() {
+    var time = performance.now() - gStartTime;
+    var frame = Math.floor(time*60.0/1000.0);
+    return frame;    
+}
+
+function killMajorNonPenta() { 
+    for(var i = 0; i < gNotes.length; i++) {
+        var note = gNotes[i];
+        if(note.isMajor && !note.isPenta) {
+            var frame = getCurrentFrame();
+            note.addAnimation(makeAnimation(
+                {
+                    "backgroundOpacity" : makeInterpolator(1.0, 0.0),
+                }
+            )
+            , frame, frame + FADE_DURATION);
+            console.log("killing note # " + i);
+        }
+    }
+}
+
+function killMinorNonPenta() { 
+    console.log("trying to kill minor non-penta");
+    for(var i = 0; i < gNotes.length; i++) {
+        var note = gNotes[i];
+        if(!note.isMajor && !note.isPenta) {
+            var frame = getCurrentFrame();
+            note.addAnimation(makeAnimation(
+                {
+                    "backgroundOpacity" : makeInterpolator(1.0, 0.0),
+                }
+            )
+            , frame, frame + FADE_DURATION);
+            console.log("killing note # " + i);
+        }
+    }
+}
 
 
 function init() {
@@ -54,70 +153,88 @@ function init() {
     }, true);    
 
     gContainer = new Container(3);
+    gNotes = [];
 
-    var xPos = QUARTERNOTE_XSTART;
+    var xPos = NOTE_XSTART;
+    var xPosDot = DOT_XSTART;
 
-    var quarterNoteWidgets = [];
-    var beatWidgets = [];
-    var eighthNoteWidgets = [];
-    var tripletNoteWidgets = [];
+    var majorHasNote = [true, false, true, false, true, true, false, true, false, true, false, true];
+    var minorHasNote = [true, false, true, true, false, true, false, true, true, false, true, false];
+    var majorBlue = [false, false, false, true, false, false, false, false, false, false, false, false];
+    var minorBlue = [false, false, false, false, false, false, true, false, false, false, false, false];
+    var majorPenta = [true, false, true, false, true, false, false, true, false, true, false, false];
+    var minorPenta = [true, false, false, true, false, true, false, true, false, false, true, false];
 
-    var barIdx = 0;
+    gNoteWidgets = [];
 
-    for(var i = 0; i < 3; i++) {
-        for(var j = 0; j < 4; j++) {
-            for(var k = 0; k < 4; k++) {    
-                var beatWidget = new TextWidget(xPos, YPOS, QUARTERNOTE_WIDTH, QUARTERNOTE_WIDTH, "" + (k+1));
+    for(var i = 0; i < 24; i++) {
+        var noteWidget = new TextWidget(xPos, YPOS, NOTE_WIDTH, NOTE_HEIGHT, "" + ((i%12)+1));
+        noteWidget.backgroundColor = "rgba(100, 100, 100, 1.0)";
+        noteWidget.backgroundOpacity = 0.4;
+        noteWidget.textY = 50;
+        gContainer.addObject(noteWidget);
+        gNoteWidgets.push(noteWidget);
 
-                if(k % 2 == 0) {
-                    beatWidget.backgroundColor = barColors[barIdx][0];//"rgba(255, 0, 0, 1.0)";
-                } else {
-                    beatWidget.backgroundColor = barColors[barIdx][1];
-                }
-
-                beatWidget.fontOpacity = 1.0;
-                beatWidget.fontSize = 40;
-                beatWidget.backgroundOpacity = 0.3;
-
-                gContainer.addObject(beatWidget);
-                beatWidgets.push(beatWidget);
-
-                xPos += QUARTERNOTE_WIDTH;
-            }
-            xPos += BAR_MARGIN;
-            barIdx++;
+        if(majorHasNote[i%12]) {
+            var majWidget = new TextWidget(xPosDot, YPOS + 100, DOT_WIDTH, DOT_WIDTH, "" );
+            majWidget.backgroundColor = "rgba(0, 255, 100, 1.0)";
+            majWidget.backgroundMode = TEXTWIDGET_BACKGROUNDMODE_CIRCLE;
+            majWidget.isMajor = true;
+            majWidget.isPenta = majorPenta[i%12];
+            majWidget.isBlue = majorBlue[i%12];
+            gContainer.addObject(majWidget);
+            gNotes.push(majWidget);
         }
-        YPOS += 200;
-        xPos = QUARTERNOTE_XSTART;
+
+        if(minorHasNote[i%12]) {
+            var minWidget = new TextWidget(xPosDot, YPOS + 160, DOT_WIDTH, DOT_WIDTH, "");
+            minWidget.backgroundColor = "rgba(255, 0, 100, 1.0)";
+            minWidget.backgroundMode = TEXTWIDGET_BACKGROUNDMODE_CIRCLE;
+            minWidget.isMajor = false;
+            minWidget.isPenta = minorPenta[i%12];
+            minWidget.isBlue = minorBlue[i%12];
+            gContainer.addObject(minWidget);
+            gNotes.push(minWidget);
+        }
+
+        xPos += NOTE_WIDTH + NOTE_MARGIN;
+        xPosDot += DOT_WIDTH + DOT_MARGIN;
     }
 
-    // Animate quarter-note pulse
+    gNoteKeyCodes = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 187];
 
-    for(var i = 0; i < 256; i++) {
-            var t1 = QUARTER_PULSE_START + 40*i - 5;
-            var t2 = QUARTER_PULSE_START + 40*i + 0;
-            var t3 = QUARTER_PULSE_START + 40*i + 35;
-            var t4 = QUARTER_PULSE_START + 40*i + 40;
+    window.addEventListener("keydown", function(evt) {
+        var code = evt.keyCode;
+        console.log(code);
 
-            var beatWidget = beatWidgets[i%(12*4)];
-            beatWidget.addAnimation(makeAnimation(
-                {
-                    "backgroundOpacity" : makeInterpolator(0.4, 0.8),
-                }
-            )
-            , t1, t2);
-
-            beatWidget.addAnimation(makeAnimation(
-                {
-                    "backgroundOpacity" : makeInterpolator(0.8, 0.4),
-                }
-            )
-            , t3, t4);
-    }
+        if(code == 219) {
+            //context.resume();
+            //vco.frequency.value = 440;
+            //vca.gain.value = 0;
+            vca.gain.value = 0;
+        }
 
 
+        if(code == 65) {
+            killMajorNonPenta();
+        } else if(code == 90) {
+            killMinorNonPenta();
+        }
+        var idx = gNoteKeyCodes.indexOf(code);
+        if(idx != -1) {
+            noteOn(idx);
+        }
+    }, false);
 
+    window.addEventListener("keyup", function(evt) {
+        var code = evt.keyCode;
+        console.log(code);
 
+        var idx = gNoteKeyCodes.indexOf(code);
+        if(idx != -1) {
+            noteOff(idx);
+        }
+    }, false);
 
     if(RECORDING) {
         //gEncoder = new Whammy.Video(60);
@@ -131,6 +248,60 @@ function init() {
         gFrame = 0;
         gCaptureStarted = false;
     }
+
+    // setup synth
+
+    context = new AudioContext;
+    vco = context.createOscillator();
+    vco.type = 'sawtooth';
+    vco.frequency.value = 200;
+
+    vco.connect(context.destination);
+    //vco.start(0);
+
+    vca = context.createGain();
+    vca.gain.value = 0;
+
+    vco.connect(vca);
+    vca.connect(context.destination);
+
+    // setup midi
+
+    navigator.requestMIDIAccess()
+        .then(onMIDISuccess, onMIDIFailure);
+
+    function onMIDISuccess(midiAccess) {
+        console.log(midiAccess);
+
+        var inputs = midiAccess.inputs;
+        var outputs = midiAccess.outputs;
+
+    for (var input of midiAccess.inputs.values())  {
+            input.onmidimessage = getMIDIMessage;
+        }        
+    }
+
+    function getMIDIMessage(midiMessage) {
+        //console.log(midiMessage);
+        var command = midiMessage.data[0];
+        var note = midiMessage.data[1];
+
+        if(command == 144) {
+            console.log("note on! " + note);
+            noteOn((note - 48));
+        } else if(command == 128) {
+            console.log("note off! " + note);
+            noteOff((note - 48));
+        }
+
+    }
+
+    function onMIDIFailure() {
+        console.log('Could not access your MIDI devices.');
+    }    
+
+
+    // start visualization
 
     window.requestAnimationFrame(render);
     gStartTime = performance.now();
@@ -153,20 +324,12 @@ function render() {
     ctx.fillRect(0, 0, gCanvas.width, gCanvas.height);
     ctx.restore();
     gContainer.render(ctx, frame);
-    if(RECORDING) {
-        //gEncoder.add(ctx);
-        
+    if(RECORDING) {        
         console.log("added frame " + frame);
         if(frame < 60*RECORDING_SECONDS) {
             window.requestAnimationFrame(render);
             gCapturer.capture(gCanvas);
         } else {
-                /*
-                gEncoder.compile(false, function(output) {
-                var url = (window.webkitURL || window.URL).createObjectURL(output);
-                console.log("DONE RECORDING!!!");
-                console.log(url);
-                window.open("www.google.com"); */
                 gCapturer.stop();
                 gCapturer.save();
 
