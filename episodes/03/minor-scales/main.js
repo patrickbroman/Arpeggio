f5410af2f0b860a210df4d066e464ebe06d79109
Patrick Broman <patrick.broman@gmail.com>
2025-01-08 21:55:11 +0100

    Added support for range widgets and other stuff

--- episodes/03/minor-scales/main.js
+++ episodes/03/minor-scales/main.js
@@ -1,8 +1,13 @@
 const SPEEDUP = 3.0;
-const NOTE_SIZE = 60;
+const NOTE_SIZE = 50;
+const NOTE_SPACING = 68;
 
 class IntervalWidget {
-    constructor(x1, x2, y, letterString) {
+    static ARC_UP = -1;
+    static ARC_DOWN = 1;
+    
+    constructor(x1, x2, y, letterString, arcDirection = IntervalWidget.ARC_UP) {
+        this.arcDirection = arcDirection;
         this.x1 = x1;
         this.x2 = x2;
         this.y = y;
@@ -34,13 +39,13 @@ class IntervalWidget {
         ctx.beginPath();
         ctx.moveTo(x1 + arcRadius, y);
         ctx.lineTo(midX - textMeasure.width/2 - 4, y);
-        ctx.arcTo(x1, y, x1, y - arcHeight, arcRadius);
+        ctx.arcTo(x1, y, x1, y  + this.arcDirection * arcHeight, arcRadius);
         ctx.stroke();
 
         ctx.beginPath();
         ctx.moveTo(x2 - arcRadius, y);
         ctx.lineTo(midX + textMeasure.width/2 + 4, y);
-        ctx.arcTo(x2, y, x2, y - arcHeight, arcRadius);
+        ctx.arcTo(x2, y, x2, y + this.arcDirection * arcHeight, arcRadius);
         ctx.stroke();
     }
 }
@@ -50,7 +55,7 @@ class Digikeys {
     static HIGHLIGHTING = 1;
 
     // Define color constants for different states
-    static COLOR_NOT_HIGHLIGHTED_NOT_PRESSED = "#1b1b1b";
+    static COLOR_NOT_HIGHLIGHTED_NOT_PRESSED = "#252525";
     static COLOR_NOT_HIGHLIGHTED_PRESSED = "#555";
     static COLOR_HIGHLIGHTED_NOT_PRESSED = "#A05F8C";
     static COLOR_HIGHLIGHTED_PRESSED = brightenColor(Digikeys.COLOR_HIGHLIGHTED_NOT_PRESSED, 0.5);
@@ -61,9 +66,11 @@ class Digikeys {
     static ACCIDENTAL_MODE_FLATS = 0;
     static ACCIDENTAL_MODE_SHARPS = 1;
 
-    static ANIM_SPEED = 3.5;
+    static ANIM_SPEED = 3.4;
 
-    constructor(midiInterface, inputInterface) {
+    constructor(y, midiInterface, inputInterface) {
+        this.y = y;
+        this.showIntervals = true;
         this.animating = false;
         this.inputMode = Digikeys.PLAYING;
         this.noteWidgets = [];
@@ -73,12 +80,14 @@ class Digikeys {
         this.intervalWidgets = [];
         this.intervalMode = Digikeys.INTERVAL_MODE_STEPS;
 
+        this.rangeWidgets = [];
+
         midiInterface.addNoteEventListener(this);
         inputInterface.addKeyEventListener(this);
 
         // Initialize two octaves of widgets
-        let xPos = 1920 / 2 - 70 * 24 + 4;
-        const xInc = 70;
+        let xPos = 1920 / 2 - NOTE_SPACING * 24 - 24;
+        const xInc = NOTE_SPACING;
         const noteSize = NOTE_SIZE;
 
         this.flatNoteNames = [
@@ -113,6 +122,17 @@ class Digikeys {
         }
     }
 
+    addRangeWidget(startNoteIdx, endNoteIdx, y, label) {
+        let nw1 = this.noteWidgets[startNoteIdx];
+        let nw2 = this.noteWidgets[endNoteIdx];
+
+        let x1 = nw1.x + NOTE_SIZE / 2;
+        let x2 = nw2.x + NOTE_SIZE / 2;
+        
+        let w = new IntervalWidget(x1, x2, y, label, IntervalWidget.ARC_DOWN);
+        this.rangeWidgets.push(w);
+    }
+
     updateNoteWidgetColor(noteWidget) {
         if (noteWidget.highlighted && noteWidget.pressed) {
             noteWidget.backgroundColor = Digikeys.COLOR_HIGHLIGHTED_PRESSED;
@@ -143,7 +163,6 @@ class Digikeys {
                 this.updateIntervals();
             }
         }
-
         // Update the widget's color based on its state
         this.updateNoteWidgetColor(noteWidget);
     }
@@ -156,7 +175,7 @@ class Digikeys {
                 if(prevHighlightedNoteWidget != null) {
                     let x1 = prevHighlightedNoteWidget.x;
                     let x2 = widget.x;
-                    let str = "";//x2 - x1 > 70 ? "W" : "H";
+                    let str = "";
                     if(this.intervalMode == Digikeys.INTERVAL_MODE_NAMES) {
                         let intervalNames = ['u', 'm2', 'M2', 'm3', 'M3', 'P4', 'd5', 'P5', 'm6', 'M6', 'm7', 'M7'];
                         str = intervalNames[widget.index - prevHighlightedNoteWidget.index];
@@ -192,13 +211,15 @@ class Digikeys {
             if(event.key === 'f') {
                 this.toggleAccidentals();
             }
-            if (event.key === "h") {
+            if (event.key === "i") {
+                console.log("TOGGLING INTERVAL MODE!");
+                this.showIntervals = !this.showIntervals;
+            } else if (event.key === "h") {
                 console.log("TOGGLING INPUT MODE!");
                 this.inputMode = this.inputMode === Digikeys.PLAYING
                     ? Digikeys.HIGHLIGHTING
                     : Digikeys.PLAYING;
-            }
-            if (event.key === "c") {
+            } else if (event.key === "c") {
                 console.log("CLEARING HIGHLIGHTS!");
                 this.noteWidgets.forEach((widget) => {
                     widget.highlighted = false;
@@ -211,14 +232,14 @@ class Digikeys {
                 console.log("STARTED ANIMATING!");
                 this.animDirection = -1;
                 this.animOffset = 0;
-                this.animFrames = 70 / Digikeys.ANIM_SPEED;
+                this.animFrames = NOTE_SPACING / Digikeys.ANIM_SPEED;
             } else if (event.key === "ArrowRight") {
                 if (this.animating) return;
                 this.animating = true;
                 console.log("STARTED ANIMATING!");
                 this.animDirection = +1;
                 this.animOffset = 0;
-                this.animFrames = 70 / Digikeys.ANIM_SPEED;
+                this.animFrames = NOTE_SPACING / Digikeys.ANIM_SPEED;
             }
         }
     }
@@ -236,6 +257,10 @@ class Digikeys {
                 widget.x1 += this.animDirection * Digikeys.ANIM_SPEED;
                 widget.x2 += this.animDirection * Digikeys.ANIM_SPEED;
             });
+            this.rangeWidgets.forEach((widget) => {
+                widget.x1 += this.animDirection * Digikeys.ANIM_SPEED;
+                widget.x2 += this.animDirection * Digikeys.ANIM_SPEED;
+            });
 
             this.animFrames--;
             if (this.animFrames === 0) {
@@ -243,16 +268,32 @@ class Digikeys {
                 console.log("STOPPED ANIMATING!");
             }
         }
+
+        ctx.save();
+
+        ctx.translate(0, this.y);
+
         this.noteWidgets.forEach((widget) => {
             widget.render(ctx, frame);
         });
 
         ctx.save();
-        this.intervalWidgets.forEach((widget) => {
-            widget.render(ctx);
-        });
+        if(this.showIntervals) {
+            this.intervalWidgets.forEach((widget) => {
+                widget.render(ctx);
+            });
+            this.rangeWidgets.forEach((widget) => {
+                widget.render(ctx);
+            });
+        }
         ctx.restore();
 
+        const lg = gImageManager.get("leftGrad.png");
+        const rg = gImageManager.get("rightGrad.png");
+        ctx.drawImage(lg, 0, 100);
+        ctx.drawImage(rg, 1920-rg.width, 100);    
+
+        ctx.restore();
     }
 }
 
@@ -271,7 +312,11 @@ async function init() {
 
     gInput = new InputInterface();
     gMidi = new MIDIInterface();
-    gDigikeys = new Digikeys(gMidi, gInput);
+    gDigikeys = new Digikeys(300, gMidi, gInput);
+
+    gDigikeys.addRangeWidget(12, 24, 24, "C major");
+    gDigikeys.addRangeWidget(24, 36, 24, "C major");
+    gDigikeys.addRangeWidget(12+9, 24+9, 72, "A minor");
 
     gStartTime = performance.now();
     window.requestAnimationFrame(render);
@@ -296,10 +341,6 @@ function render() {
         1060
     );
 
-    const lg = gImageManager.get("leftGrad.png");
-    const rg = gImageManager.get("rightGrad.png");
-    ctx.drawImage(lg, 0, 100);
-    ctx.drawImage(rg, 1920-rg.width, 100);
 
     window.requestAnimationFrame(render);
 }
